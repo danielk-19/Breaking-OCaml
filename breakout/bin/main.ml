63d3e0afbf1f9245e2035951a1cbae7f49abcfe2
Daniel Ketema <dketema34@gmail.com>
2026-06-24 12:18:21 -0400

    collision detection can be confusing...

--- breakout/bin/main.ml
+++ breakout/bin/main.ml
@@ -26,8 +26,8 @@ let setup () =
 
   let ball =
     let rad = 10. in
-    let pos_x = ((float_of_int screen_width) /. 2. -. rad) in
-    let pos_y = ((float_of_int screen_height) /. 2. -. rad) in
+    let pos_x = ((float_of_int screen_width) /. 2.) in
+    let pos_y = ((float_of_int screen_height) /. 2.) in
 
     let position = Vector2.create pos_x pos_y in
     let velocity = Vector2.create 5. 5. in
@@ -35,7 +35,24 @@ let setup () =
     {Objs.Ball.position; velocity; radius}
   in
 
-  {Objs.State.paddle; ball; pause = true; frames_counter = 0}
+  let blocks = 
+    let dim_x = 200. in
+    let dim_y = 100. in
+    let num_blocks_x = 4 in
+    let num_blocks_y = 2 in
+    let offset = 5. in
+    let rec make_blocks x y acc =
+      if y >= (dim_y *. float_of_int num_blocks_y +. offset *. float_of_int num_blocks_y +. 50.) then acc
+      else if x >= (dim_x *. float_of_int num_blocks_x +. offset *. float_of_int num_blocks_x +. (float_of_int screen_width -. (dim_x *. float_of_int num_blocks_x +. offset *. float_of_int num_blocks_x)) /. 2.) then
+        make_blocks ((float_of_int screen_width -. (dim_x *. float_of_int num_blocks_x +. offset *. float_of_int num_blocks_x)) /. 2.) (y +. dim_y +. offset) acc
+      else
+        let block = {Objs.Block.position = Vector2.create x y; dimensions = Vector2.create dim_x dim_y; color = Raylib.Color.red} in
+        make_blocks (x +. dim_x +. offset) y (block :: acc)
+    in
+    make_blocks ((float_of_int screen_width -. (dim_x *. float_of_int num_blocks_x +. offset *. float_of_int num_blocks_x)) /. 2.) 100. []
+  in
+
+  {Objs.State.paddle; ball; blocks; pause = true; score = 0}
 
 let rec loop (state : Objs.State.t) =
   match (Raylib.window_should_close ()) with
@@ -56,8 +73,9 @@ let rec loop (state : Objs.State.t) =
         let {Objs.Paddle.position; velocity; dimensions} = state.paddle in
 
         let velocity =
-          if (is_key_pressed_repeat Key.Left && (Vector2.x position) > 0.) then -15.
-          else if (is_key_pressed_repeat Key.Right && (Vector2.x position) < ((float_of_int screen_width) -. (Vector2.x dimensions))) then 15.
+          if (is_key_down Key.Left && (Vector2.x position) > 0.) then -15.
+          else if (is_key_down Key.Right &&
+            (Vector2.x position) < ((float_of_int screen_width) -. (Vector2.x dimensions))) then 15.
           else 0.
         in
 
@@ -80,16 +98,20 @@ let rec loop (state : Objs.State.t) =
         let velocity = 
           let vx =
             if (Vector2.x position <= float_of_int radius ||
-            Vector2.x position >= float_of_int (screen_width - radius)) then
+            Vector2.x position >= float_of_int (screen_width - radius) (*||
+            ((((Vector2.x position +. float_of_int radius) -. Vector2.x state.paddle.position) = 0. &&
+            ((Vector2.x position -. float_of_int radius) -. (Vector2.x state.paddle.position +. Vector2.x state.paddle.dimensions)) = 0.) &&
+            ((Vector2.y position +. float_of_int radius) >= Vector2.y state.paddle.position &&
+            (Vector2.y position -. float_of_int radius) <= (Vector2.y state.paddle.position +. Vector2.y state.paddle.dimensions)))*)) then
               -.(Vector2.x velocity)
             else (Vector2.x velocity)
           in
 
           let vy =
             if (Vector2.y position <= float_of_int radius ||
-            Vector2.y position >= float_of_int (screen_height - radius) ||
             (((Vector2.y position +. float_of_int radius) -. Vector2.y state.paddle.position) >= 0. &&
-            ((Vector2.x position +. float_of_int radius) >= Vector2.x state.paddle.position && (Vector2.x position -. float_of_int radius) <= (Vector2.x state.paddle.position +. Vector2.x state.paddle.dimensions)))) then
+            ((Vector2.x position +. float_of_int radius) >= Vector2.x state.paddle.position &&
+            (Vector2.x position -. float_of_int radius) <= (Vector2.x state.paddle.position +. Vector2.x state.paddle.dimensions)))) then
               -.(Vector2.y velocity)
             else (Vector2.y velocity)
           in
@@ -97,13 +119,44 @@ let rec loop (state : Objs.State.t) =
           Vector2.create vx vy
         in
 
-        let position = Vector2.create
-          (Vector2.x position +. Vector2.x velocity)
-          (Vector2.y position +. Vector2.y velocity)
+        let position =
+          if ((Vector2.y position +. float_of_int radius) >= float_of_int screen_height) then
+            Vector2.create (float_of_int screen_width /. 2.) (float_of_int screen_height /. 2.)
+          else
+            Vector2.create (Vector2.x position +. Vector2.x velocity) (Vector2.y position +. Vector2.y velocity)
         in
 
+        let rec check_block_collision blocks =
+          match blocks with
+          | [] -> velocity
+          | block :: rest ->
+            let vx =
+              if (Objs.State.collision_x state.ball block) then
+                -.(Vector2.x velocity)
+              else (Vector2.x velocity)
+            in
+
+            let vy =
+              if (Objs.State.collision_y state.ball block) then
+                -.(Vector2.y velocity)
+              else (Vector2.y velocity)
+            in
+
+            let new_velocity = Vector2.create vx vy in
+            if new_velocity <> velocity then new_velocity else check_block_collision rest
+        in
+
+        let velocity = check_block_collision state.blocks in
+
+        let blocks = List.filter (fun (block : Objs.Block.t) ->
+          not (
+            (Objs.State.collision_x state.ball block) &&
+            (Objs.State.collision_y state.ball block)
+          )
+        ) state.blocks in
+
         let ball = {Objs.Ball.position; velocity; radius} in
-        {state with ball}
+        {state with ball; blocks; score = state.score + (List.length state.blocks - List.length blocks)}
     in
 
     Objs.State.draw state;
--- breakout/lib/objects.ml
+++ breakout/lib/objects.ml
@@ -18,30 +18,91 @@ module Paddle =
         }
     end
 
+module Block =
+    struct
+        type t = {
+            position : Raylib.Vector2.t;
+            dimensions : Raylib.Vector2.t;
+            color : Raylib.Color.t;
+        }
+    end
+
 module State =
     struct
         type t = {
             paddle : Paddle.t;
             ball : Ball.t;
+            blocks : Block.t list;
             pause : bool;
-            frames_counter: int
+            score: int
         }
-        let draw {paddle; ball; pause; frames_counter} =
+        
+        (* Fixing Error Currently *)
+        let collision (ball : Ball.t) (block : Block.t) =
+            let open Raylib in
+            (((Vector2.y ball.position +. float_of_int ball.radius) >= Vector2.y block.position &&
+            (Vector2.y ball.position -. float_of_int ball.radius) <= (Vector2.y block.position +. Vector2.y block.dimensions)) &&
+            ((Vector2.x ball.position +. float_of_int ball.radius) >= Vector2.x block.position &&
+            (Vector2.x ball.position -. float_of_int ball.radius) <= (Vector2.x block.position +. Vector2.x block.dimensions)))
+
+        let collision_x (ball : Ball.t) (block : Block.t) =
+            let open Raylib in
+            collision ball block (*&&
+            (Vector2.x ball.position +. float_of_int ball.radius -. Vector2.x block.position >= Vector2.y ball.position +. float_of_int ball.radius -. Vector2.y block.position &&
+            Vector2.x ball.position +. float_of_int ball.radius -. Vector2.x block.position >= Vector2.y block.position +. Vector2.y block.dimensions -. Vector2.y ball.position -. float_of_int ball.radius) ||
+            (Vector2.x block.position +. Vector2.x block.dimensions -. Vector2.x ball.position -. float_of_int ball.radius >= Vector2.y ball.position +. float_of_int ball.radius -. Vector2.y block.position &&
+            Vector2.x block.position +. Vector2.x block.dimensions -. Vector2.x ball.position -. float_of_int ball.radius >= Vector2.y block.position +. Vector2.y block.dimensions -. Vector2.y ball.position -. float_of_int ball.radius)
+            *)
+        let collision_y (ball : Ball.t) (block : Block.t) =
+            let open Raylib in
+            collision ball block (*&&
+            (Vector2.y ball.position +. float_of_int ball.radius -. Vector2.y block.position >= Vector2.x ball.position +. float_of_int ball.radius -. Vector2.x block.position &&
+            Vector2.y ball.position +. float_of_int ball.radius -. Vector2.y block.position >= Vector2.x block.position +. Vector2.x block.dimensions -. Vector2.x ball.position -. float_of_int ball.radius) ||
+            (Vector2.y block.position +. Vector2.y block.dimensions -. Vector2.y ball.position -. float_of_int ball.radius >= Vector2.x ball.position +. float_of_int ball.radius -. Vector2.x block.position &&
+            Vector2.y block.position +. Vector2.y block.dimensions -. Vector2.y ball.position -. float_of_int ball.radius >= Vector2.x block.position +. Vector2.x block.dimensions -. Vector2.x ball.position -. float_of_int ball.radius)
+            *)
+        let draw {paddle; ball; blocks; pause; score} =
             let open Raylib in
             begin_drawing ();
 
             clear_background Color.darkgray;
             draw_rectangle_v paddle.position paddle.dimensions Color.white;
             draw_circle_v ball.position (float_of_int ball.radius) Color.white;
+            
+            List.iter (fun (block : Block.t) ->
+                draw_rectangle_v block.position block.dimensions block.color
+            ) blocks;
 
-            if pause then
-                let text = "Press Up Arrow To Continue" in
-                let font_size = 36 in
-                let font_width = (measure_text text font_size) in
-                draw_text text
-                    (get_screen_width () / 2 - font_width / 2) (get_screen_height () / 4)
-                    font_size Color.white;
-            else ();
+            let text =
+                if pause then "Press Up Arrow To Continue"
+                else ""
+            in
+            let font_size = 48 in
+            let font_width = (measure_text text font_size) in            
+            draw_text text
+                (get_screen_width () / 2 - font_width / 2) (get_screen_height () / 4)
+                font_size Color.white;
 
+            let text = "Score: " ^ string_of_int score in
+            let font_size = 24 in
+            let font_width = (measure_text text font_size) in
+            draw_text text
+                (get_screen_width () / 2 - font_width / 2) 25
+                font_size Color.white;
+            
+            let dims = 50 in
+            let start_pos_x = get_screen_width () - 25 in
+            let start_pos_y = 25 in
+
+            let start_pos = Vector2.create (float_of_int (start_pos_x - dims)) (float_of_int start_pos_y) in
+            let end_pos = Vector2.create (float_of_int start_pos_x) (float_of_int (start_pos_y + dims)) in
+
+            draw_line_ex start_pos end_pos 10. Color.white;
+            
+            let start_pos = Vector2.create (float_of_int (start_pos_x - dims)) (float_of_int (start_pos_y + dims)) in
+            let end_pos = Vector2.create (float_of_int start_pos_x) (float_of_int start_pos_y) in
+            
+            draw_line_ex start_pos end_pos 10. Color.white;
+            
             end_drawing ()
     end
